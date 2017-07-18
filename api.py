e6b18118a892a94269c8c75b895af7dfa0b03e6f
Jean-Mark Wright <jeanmark.wright@gmail.com>
2017-07-18 01:44:14 -0500

    Added some imports and configured the app to display static files in uploads folder.

--- api.py
+++ api.py
@@ -1,9 +1,24 @@
-from flask import Flask, request
-from flask import render_template
+import uuid
+import os
+
+from flask import Flask, request, flash, url_for
+from flask import render_template, redirect, jsonify
+from werkzeug import SharedDataMiddleware
+import cv2
+import pytesseract
+from PIL import Image
+
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.add_url_rule('/preview/<image_id>', 'preview',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
